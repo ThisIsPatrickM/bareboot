afaf77a175c43c9dc008f161939e3ca631680f36
Patrick Molz <patrick.molz@tum.de>
2022-07-30 14:12:18 +0200

    pipeline test

--- tests/common_tests/common.cpp
+++ tests/common_tests/common.cpp
@@ -7,13 +7,5 @@ using namespace bootloader;
 
 TEST(Basic, Empty)
 {
-    GlobalImageMetadata img;
-    // EXPECT_EQ(img.currentImage, 0);
-}
-
-TEST(Basic, MetadataGlobal)
-{
-    // EXPECT_EQ(blTable.globalImageMetadata.bootcounter, 0);
-    // blTable.globalImageMetadata.bootcounter++;
-    // EXPECT_EQ(blTable.globalImageMetadata.bootcounter, 1);
+    EXPECT_EQ(true, true);
 }
